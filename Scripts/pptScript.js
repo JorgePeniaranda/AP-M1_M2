88c7ee549399b93be189d27a192ad84b21e7dfa3
Jorgemaxii25 <jorge.maxii25@gmail.com>
2021-07-03 13:29:43 -0300

    Entregas-M1-y-M2

---
+++ Scripts/pptScript.js
@@ -0,0 +1,36 @@
+function PiedraPapelTijera() {
+    alert("¡Jugaras contra la Computadora un Piedra, Papel o Tijera!");
+    sessionStorage.setItem('opUser', opUser);
+    location.reload();
+}
+
+var opUser;
+var opciones = ["Piedra", "Papel", "Tijera"];
+var userElecc = opciones[sessionStorage.getItem('opUser')];
+var computerElecc = opciones[Math.round(Math.random() * 2)];
+
+
+document.write('<section id="Resultado">');
+if (userElecc == "Piedra" && computerElecc == "Papel") {
+    document.write('<h1 class="Perdiste">Perdiste</h1>' + '<p>Elegiste "' + userElecc + '" y la Computadora eligio "' + computerElecc + '".<p>');
+}
+else if (userElecc == "Papel" && computerElecc == "Piedra") {
+    document.write('<h1 class="Ganaste">Ganaste</h1>' + '<p>Elegiste "' + userElecc + '" y la Computadora eligio "' + computerElecc + '".<p>');
+}
+else if (userElecc == "Tijera" && computerElecc == "Piedra") {
+    document.write('<h1 class="Perdiste">Perdiste</h1>' + '<p>Elegiste "' + userElecc + '" y la Computadora eligio "' + computerElecc + '".<p>');
+}
+else if (userElecc == "Piedra" && computerElecc == "Tijera") {
+    document.write('<h1 class="Ganaste">Ganaste</h1>' + '<p>Elegiste "' + userElecc + '" y la Computadora eligio "' + computerElecc + '".<p>');
+}
+else if (userElecc == "Papel" && computerElecc == "Tijera") {
+    document.write('<h1 class="Perdiste">Perdiste</h1>' + '<p>Elegiste "' + userElecc + '" y la Computadora eligio "' + computerElecc + '".<p>');
+}
+else if (userElecc == "Tijera" && computerElecc == "Papel") {
+    document.write('<h1 class="Ganaste">Ganaste</h1>' + '<p>Elegiste "' + userElecc + '" y la Computadora eligio "' + computerElecc + '".<p>');
+}
+else if (userElecc == computerElecc) {
+    document.write('<h1>Empate</h1>' + '<p>Elegiste "' + userElecc + '" y la Computadora eligio "' + computerElecc + '".<p>');
+}
+document.write('</section>');
+sessionStorage.clear()
